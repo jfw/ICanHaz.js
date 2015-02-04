15295e82f96f411cb5165e787872fda345d46c6a
Jeff Fein-Worton <jeff@fein-worton.com>
2015-02-03 16:32:14 -0800

    trigger 'template-loaded' event when template is loaded

--- ICanHaz.js
+++ ICanHaz.js
@@ -482,6 +482,9 @@ var Mustache = function () {
                     var result = Mustache.to_html(ich.templates[name], data, ich.templates);
                     return (ich.$ && !raw) ? ich.$(trim(result)) : result;
                 };
+                if (typeof ich.$ !== 'undefined') {
+                    $(document).trigger('template-loaded', [name]);
+                }
             }
         },
 
